fd94a5743b008d6e15f9d02e787219637ebdb96a
Abigail <161676508+Abigail-fs@users.noreply.github.com>
2024-05-30 19:04:53 -0700

    Add files via upload
    added soil stuff

--- code/main.py
+++ code/main.py
@@ -1,29 +1,26 @@
-import pygame
-import sys
+import pygame, sys
 from settings import *
 from level import Level
 
-
 class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.clock = pygame.time.Clock()
-        pygame.display.set_caption("Starfire Canyon ")
-        self.level = Level()
+	def __init__(self):
+		pygame.init()
+		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+		pygame.display.set_caption('Sprout land')
+		self.clock = pygame.time.Clock()
+		self.level = Level()
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-            dt = self.clock.tick() / 1000
-            self.level.run(dt)
-            pygame.display.update()
-
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+  
+			dt = self.clock.tick() / 1000
+			self.level.run(dt)
+			pygame.display.update()
 
 if __name__ == '__main__':
-    game = Game()
-    game.run()
+	game = Game()
+	game.run()
--- code/sprites.py
+++ code/sprites.py
@@ -5,115 +5,123 @@ from timer import Timer
 
 
 class Generic(pygame.sprite.Sprite):
-    def __init__(self, pos, surf, groups, z=LAYERS['main']):
-        super().__init__(groups)
-        self.image = surf
-        self.rect = self.image.get_rect(topleft=pos)
-        self.z = z
-        self.hitbox = self.rect.copy().inflate(-self.rect.width * 0.2, -self.rect.height * 0.75)
+	def __init__(self, pos, surf, groups, z = LAYERS['main']):
+		super().__init__(groups)
+		self.image = surf
+		self.rect = self.image.get_rect(topleft = pos)
+		self.z = z
+		self.hitbox = self.rect.copy().inflate(-self.rect.width * 0.2, -self.rect.height * 0.75)
+
+
+class Interaction(Generic):
+	def __init__(self, pos, size, groups, name):
+		surf = pygame.Surface(size)
+		super().__init__(pos, surf, groups)
+		self.name = name
 
 
 class Water(Generic):
-    def __init__(self, pos, frames, groups):
+	def __init__(self, pos, frames, groups):
 
-        # animation setup
-        self.frames = frames
-        self.frame_index = 0
+		#animation setup
+		self.frames = frames
+		self.frame_index = 0
 
-        # sprite setup
-        super().__init__(pos=pos, surf= self.frames[self.frame_index], groups=groups, z=LAYERS['water'])
+		# sprite setup
+		super().__init__(
+				pos = pos, 
+				surf = self.frames[self.frame_index], 
+				groups = groups, 
+				z = LAYERS['water']) 
 
-    def animate(self, dt):
-        self.frame_index += 5 * dt
-        if self.frame_index >= len(self.frames):
-            self.frame_index = 0
-        self.image = self.frames[int(self.frame_index)]
+	def animate(self,dt):
+		self.frame_index += 5 * dt
+		if self.frame_index >= len(self.frames):
+			self.frame_index = 0
+		self.image = self.frames[int(self.frame_index)]
 
-    def update(self, dt):
-        self.animate(dt)
+	def update(self,dt):
+		self.animate(dt)
 
 
 class WildFlower(Generic):
-    def __init__(self, pos, surf, groups):
-        super().__init__(pos, surf, groups)
-        self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.9)
+	def __init__(self, pos, surf, groups):
+		super().__init__(pos, surf, groups)
+		self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.9)
 
 
 class Particle(Generic):
-    def __init__(self, pos, surf, groups, z, duration=200):
-        super().__init__(pos, surf, groups, z)
-        self.start_time = pygame.time.get_ticks()
-        self.duration = duration
+	def __init__(self, pos, surf, groups, z, duration=200):
+		super().__init__(pos, surf, groups, z)
+		self.start_time = pygame.time.get_ticks()
+		self.duration = duration
 
-        # white surface
+		# white surface 
+		mask_surf = pygame.mask.from_surface(self.image)
+		new_surf = mask_surf.to_surface()
+		new_surf.set_colorkey((0, 0, 0))
+		self.image = new_surf
 
-        mask_surf = pygame.mask.from_surface(self.image)
-        new_surf = mask_surf.to_surface()
-        new_surf.set_colorkey((0, 0, 0))
-        self.image = new_surf
-
-    def update(self, dt):
-        current_time = pygame.time.get_ticks()
-        if current_time - self.start_time > self.duration:
-            self.kill()
+	def update(self, dt):
+		current_time = pygame.time.get_ticks()
+		if current_time - self.start_time > self.duration:
+			self.kill()
 
 
 class Tree(Generic):
-    def __init__(self, pos, surf, groups, name, player_add):
-        super().__init__(pos, surf, groups)
+	def __init__(self, pos, surf, groups, name, player_add):
+		super().__init__(pos, surf, groups)
 
-        # tree attributes
-        self.health = 5
-        self.alive = True
-        stump_path = f'../graphics/stumps/{"small" if name == "Small" else "large"}.png'
-        self.stump_surf = pygame.image.load(stump_path).convert_alpha()
-        self.invul_timer = Timer(200)
+		# tree attributes
+		self.health = 5
+		self.alive = True
+		stump_path = f'../graphics/stumps/{"small" if name == "Small" else "large"}.png'
+		self.stump_surf = pygame.image.load(stump_path).convert_alpha()
+		self.invul_timer = Timer(200)
 
-        # apples
-        self.apple_surf = pygame.image.load('../graphics/fruit/apple.png').convert_alpha()
-        self.apple_pos = APPLE_POS[name]
-        self.apple_sprites = pygame.sprite.Group()
-        self.create_fruit()
+		# apples
+		self.apple_surf = pygame.image.load('../graphics/fruit/apple.png')
+		self.apple_pos = APPLE_POS[name]
+		self.apple_sprites = pygame.sprite.Group()
+		self.create_fruit()
 
-        self.player_add = player_add
+		self.player_add = player_add
 
-    def damage(self):
+	def damage(self):
+		
+		# damaging the tree
+		self.health -= 1
+		# remove an apple
+		if len(self.apple_sprites.sprites()) > 0:
+			random_apple = choice(self.apple_sprites.sprites())
+			Particle(
+				pos=random_apple.rect.topleft,
+				surf=random_apple.image,
+				groups = self.groups()[0], 
+				z = LAYERS['fruit'])
+			self.player_add('apple')
+			random_apple.kill()
 
-        # damaging the tree
-        self.health -= 1
-        # """ this is so fucking stupid why is delta time like this """
-        # print(" - hp")
-        # remove an apple
-        if len(self.apple_sprites.sprites()) > 0:
-            # """ what the fuck why does a print statement make the code run """
-            print('apple')
-            random_apple = choice(self.apple_sprites.sprites())
-            Particle(pos=random_apple.rect.topleft,
-                     surf=random_apple.image,
-                     groups=self.groups()[0],
-                     z=LAYERS['fruit'])
-            self.player_add('apple')
-            random_apple.kill()
-    def death_check(self):
+	def check_death(self):
+		if self.health <= 0:
+			Particle(self.rect.topleft, self.image, self.groups()[0], LAYERS['fruit'], 300)
+			self.image = self.stump_surf
+			self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+			self.hitbox = self.rect.copy().inflate(-10,-self.rect.height * 0.6)
+			self.alive = False
+			self.player_add('wood')
 
-        if self.health <= 0:
-            Particle(self.rect.topleft, self.image, self.groups()[0], LAYERS['fruit'], duration=300)
-            self.image = self.stump_surf
-            self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
-            self.hitbox = self.rect.copy().inflate(-10, -self.rect.height * 0.6)
-            self.alive = False
-            self.player_add('wood')
+	def update(self,dt):
+		if self.alive:
+			self.check_death()
 
-    def update(self, dt):
-        if self.alive:
-            self.death_check()
-
-    def create_fruit(self):
-
-        for pos in self.apple_pos:
-            if randint(0, 10) < 2:
-                # print('apple made') # DO NOT REMOVE THIS PRINT STATEMENT
-                                    # no idea why but if you do it will fuck the apples up
-                x = pos[0] + self.rect.left
-                y = pos[1] + self.rect.top
-                Generic((x, y), self.apple_surf, [self.apple_sprites, self.groups()[0]], z=LAYERS['fruit'])
+	def create_fruit(self):
+		for pos in self.apple_pos:
+			if randint(0,10) < 2:
+				x = pos[0] + self.rect.left
+				y = pos[1] + self.rect.top
+				Generic(
+					pos=(x, y),
+					surf=self.apple_surf,
+					groups = [self.apple_sprites,self.groups()[0]],
+					z=LAYERS['fruit'])
